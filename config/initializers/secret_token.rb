57268f9dbb35819d7a4981a2d8b7230e75a6f9b4
romansador <jronyagz.ixi@gmail.com>
2015-11-09 10:22:16 +0000

    update some configurations

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GameFowl::Application.config.secret_token = '372024e02ad03994b9d7bdc0e718b691d95854b9fad5164543a063147bb7f81e9e61db24624a59f4f17065684869e4d29d1ed843494c3ec46d0e04264d84198d'
+Dwbh::Application.config.secret_token = '372024e02ad03994b9d7bdc0e718b691d95854b9fad5164543a063147bb7f81e9e61db24624a59f4f17065684869e4d29d1ed843494c3ec46d0e04264d84198d'
