cc556bde2b6dbe62c783f82755ee6a663be9d87c
romansador <jronyagz.ixi@gmail.com>
2015-11-23 09:07:05 +0000

    update google login issue

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '113961971625-usdatd2fea8t3a2lnrmac1fkiebuvjgb.apps.googleusercontent.com', 'vS4Okug8oiyp5OhZiFy0RRL9'
-end
