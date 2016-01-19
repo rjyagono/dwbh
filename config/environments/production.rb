3ca7f0acf39d389a5563da5dcea207dd1a504ce2
romansador <jronyagz.ixi@gmail.com>
2016-01-19 07:46:26 +0000

    added a log rotator in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 'daily')
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 2, 1024000)
 end
